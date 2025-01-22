a8ddcb969baa5b0867539c8187acb4229902ef9a
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-22 18:57:00 +1100

    added movement to flying eyes

--- Game Code/Flying_Eye.py
+++ Game Code/Flying_Eye.py
@@ -2,14 +2,18 @@ import pygame
 from Configuration import *
 
 class Flying_Eye():
-    def __init__(self, position):
+    def __init__(self, position, move_right):
         
         flying_eyeImage = pygame.image.load(SPRITESHEET_PATH + "/Enemies/Flying Eye/Fly/Flight.png")
         self.image = flying_eyeImage.subsurface(pygame.Rect(16, 0, 47, 47))
         self.rect = self.image.get_rect(bottomleft = position)
+        self.move_right = move_right
     
     def update(self, level):
-        pass
-    
+        if self.move_right == False:
+            self.rect.x -= SPEED_FLYINGEYE
+        else:
+            self.rect.x += SPEED_FLYINGEYE
+            
     def draw(self, displayWindow):
         displayWindow.blit(self.image, self.rect)
--- Game Code/ModLevel.py
+++ Game Code/ModLevel.py
@@ -8,15 +8,19 @@ class Level():
     def __init__(self, displayWindow):
         
         self.background = Background()
-        self.flying_eye1 = Flying_Eye((200, 200))
+        self.flying_eye1 = Flying_Eye((200, 200), move_right = True)
+        self.flying_eye2 = Flying_Eye((300, 300), move_right = False)
         self.displayWindow = displayWindow
         
     def update(self):
         self.flying_eye1.update(self)
+        self.flying_eye2.update(self)
+    
     
     def draw(self):
         self.background.draw(self.displayWindow)
         self.flying_eye1.draw(self.displayWindow)
+        self.flying_eye2.draw(self.displayWindow)
     
     def run(self):
         self.update()
