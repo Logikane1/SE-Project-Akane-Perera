4d412745a3ed3955e36ab41fccb6a4b15273481d
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-22 16:26:17 +1100

    Setting up the application window

---
+++ ModLevel.py
@@ -0,0 +1,17 @@
+# module for level class
+import pygame
+from Configuration import *
+
+class Level():
+    def __init__(self, game_window):
+        self.game_window = game_window
+        
+    def update(self):
+        pass
+    
+    def draw(self):
+        pass
+    
+    def run(self):
+        self.update()
+        self.run()
--- main_game.py
+++ main_game.py
@@ -1,3 +1,29 @@
 import pygame
-from configuration import *
+from Configuration import *
+from ModLevel import Level
 
+
+
+pygame.init()
+time = pygame.time.Clock()
+
+
+level = Level(game_window)
+game_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("The Archon")
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run == False
+                
+    pygame.display.flip()
+    time.tick(60)
+    
+pygame.quit()
+    
