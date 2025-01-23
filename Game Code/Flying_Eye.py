34a679e2f9330cda68fc95f737729cf4f741a299
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-23 16:57:12 +1100

    more progress

--- Game Code/Flying_Eye.py
+++ Game Code/Flying_Eye.py
@@ -11,9 +11,20 @@ Flying_EyeSprites = [
     (256, 8, 47, 47),
     (320, 8, 47, 47),
     (384, 8, 47, 47),
-    (448, 8, 47, 47),
+    (448, 8, 47, 47)
 ]
 
+
+Flying_Eye_AttackSprites = [
+    (0, 0, 52, 52),
+    (52, 0, 52, 52),
+    (104, 0, 52, 52),
+    (156, 0, 52, 52),
+    (208, 0, 52, 52),
+    (260, 0, 52, 52),
+    (312, 0, 52, 52),
+    (364, 0, 52, 52)
+]
 
 
 class Flying_Eye():
@@ -21,7 +32,7 @@ class Flying_Eye():
         
         
         self.eyeSpritesheet = Spritesheet(SPRITESHEET_PATH + "/Enemies/Flying Eye/Fly/Flight.png", Flying_EyeSprites)
-        self.attackSpriteSheet = Spritesheet(SPRITESHEET_PATH + "/Enemies/Flying Eye/Attack/Enemy Attack 1.png", Flying_EyeSprites)
+        self.attackSpriteSheet = Spritesheet(SPRITESHEET_PATH + "/Enemies/Flying Eye/Attack/Enemy Attack 1.png", Flying_Eye_AttackSprites)
         
         self.image = self.eyeSpritesheet.getSprites(move_right)[0]
         self.rect = self.image.get_rect(bottomleft = position)
@@ -41,11 +52,42 @@ class Flying_Eye():
             self.move_right = True
         if self.rect.left > WINDOW_WIDTH:
             self.move_right = False
+            
+        mainCharacterRect = level.mc.rect
+        mainCharacterX = mainCharacterRect.centerx
+        
+        if self.currentState == "FLY":
+            if mainCharacterRect.top < self.rect.bottom <= mainCharacterRect.bottom:
+                if self.moving_right == True:
+                    if self.rect.left < mainCharacterX and self.rect.right > mainCharacterX - 50:
+                        self.currentState = "ATTACK"
+                        self.animationCount = 0
+                else:
+                    if self.rect.right > mainCharacterX and self.rect.left < mainCharacterX + 50:
+                        self.currentState = "ATTACK"
+                        self.animationCount = 0
+        elif self.currentState == "ATTACK":
+            if self.moving_right == True:
+                if self.rect.left >= mainCharacterX or self.rect.right < mainCharacterX - 50:
+                    self.currentState = "FLY"
+                    self.animationCount = 0
+            else:
+                if self.rect.right <= mainCharacterX or self.rect.left > mainCharacterX + 50:
+                    self.currentState = "FLY"
+                    self.animationCount = 0
+            
+            
                 
         self.selectAnimation() 
+        
+        
         self.animationCount += self.animationSpeed  # each times animation is updated, the count is increased by 1 to the next frame
         if self.animationCount >= len(self.currentAnimation): # after reaching the last frame, it goes back to the original first frame [0]
-            self.animationCount = 0
+            if self.currentState == "ATTACK":
+                self.animationCount = len(self.currentAnimation) - 1
+            else:
+                self.currentState == "FLY"
+                self.animationCount = 0
             
         self.image = self.currentAnimation[int(self.animationCount)]
             
