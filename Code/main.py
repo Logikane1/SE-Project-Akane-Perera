75c5dcf07fa0503e0ba7903b06d83a6d539a64a4
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-24 11:39:45 +1100

    Made Game Window and Added Basic Files

---
+++ Code/main.py
@@ -0,0 +1,20 @@
+from gameSettings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.displayWindow = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("THE ARCHON")
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
