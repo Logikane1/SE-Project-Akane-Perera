88c99abdf31feef79d08b5e2015331488e6bdf87
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-25 19:22:14 +1100

    .

--- Code/player.py
+++ Code/player.py
@@ -7,7 +7,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites, semicollision_sprites):
         super().__init__(groups)
         
-        self.image = pygame.image.load(join('Graphics', 'player', 'idle', '0.png'))
+        self.image = pygame.image.load(join('Graphics', 'player', 'idle', 'Archon Player Idle .png'))
         
         self.rect = self.image.get_frect(topleft = pos)
         self.previousRect = self.rect.copy
