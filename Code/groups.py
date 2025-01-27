7132bada3a2eccb1b29b7cf6e0bcb9f11d99a8b7
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-01-27 17:36:41 +1100

    - Added scrolling background for vertical movements of players

--- Code/groups.py
+++ Code/groups.py
@@ -8,6 +8,7 @@ class AllSprites(pygame.sprite.Group):
         
     def draw(self, target_position):
         self.offset.x = -(target_position[0] - WINDOW_WIDTH / 2) # creating the offset for the screen that follows the player 
+        self.offset.y = -(target_position[1] - WINDOW_HEIGHT / 2)
         for sprite in self: # returns all sprites inside of it
             offset_position = sprite.rect.topleft + self.offset
             self.displaySurface.blit(sprite.image, offset_position)
