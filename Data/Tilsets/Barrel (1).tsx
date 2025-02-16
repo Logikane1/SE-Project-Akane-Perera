26ff7eb609158c90209af284a08f76d8550e404e
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-02-17 09:22:44 +1100

    added objects (demo assets such as crate and barrel)

---
+++ Data/Tilsets/Barrel (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1.1" name="Barrel (1)" tilewidth="46" tileheight="50" tilecount="1" columns="1">
+ <image source="../../Graphics/objects/Barrel (1).png" width="46" height="50"/>
+</tileset>
---
+++ Data/Tilsets/Crate (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1.1" name="Crate (1)" tilewidth="50" tileheight="36" tilecount="1" columns="1">
+ <image source="../../Graphics/objects/Crate (1).png" width="50" height="36"/>
+</tileset>
