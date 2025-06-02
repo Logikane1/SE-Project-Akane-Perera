d64f27a4a63c360b9529d53fbfd9e2a65c32e86b
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-06-02 22:06:26 +1000

    .

---
+++ Data/Tilsets/bg_barrel_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1.1" name="bg_barrel_2" tilewidth="42" tileheight="44" tilecount="1" columns="1">
+ <image source="../../Graphics/objects/bg_barrel_2.png" width="42" height="44"/>
+</tileset>
