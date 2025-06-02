ca0a791164a5724f500c72b6af8915ae9b919a60
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-06-02 21:54:12 +1000

    .

---
+++ Data/Tilsets/door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1.1" name="door" tilewidth="82" tileheight="98" tilecount="1" columns="1">
+ <image source="../../Graphics/objects/door.png" width="82" height="98"/>
+</tileset>
