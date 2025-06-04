bdee972188d24699d6ce96586250f6bdf4d40349
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-06-04 23:44:19 +1000

    tooth (enemy) is now has running animation

--- Code/enemies.py
+++ Code/enemies.py
@@ -1,4 +1,5 @@
 from gameSettings import * 
+from random import choice
 
 class Tooth(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups, collision_sprites):
@@ -7,3 +8,12 @@ class Tooth(pygame.sprite.Sprite):
         self.image = self.frames[self.frame_index]
         self.rect = self.image.get_frect(topleft = pos)
         self.z = Z_LAYERS['main']
+        
+        self.direction = choice((-1,1))
+        self.collision_rects = [sprite.rect for sprite in collision_sprites]
+        
+    def update(self,dt):
+        
+        #animate
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[int(self.frame_index % len(self.frames))]
