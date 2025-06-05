0227e064f005ce5863dd2c64e5f9062caf56125d
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-06-06 00:14:22 +1000

    Heart icons now individually animate at random times (looks cleaner)

--- Code/ui.py
+++ Code/ui.py
@@ -1,5 +1,6 @@
 from gameSettings import *
 from sprites import AnimatedSprite
+from random import randint
 
 class UI:
     def __init__(self, font, frames):
@@ -29,4 +30,19 @@ class UI:
 class Heart(AnimatedSprite):
     def __init__(self, pos, frames, groups):
         super().__init__(pos, frames, groups)
+        self.active = False
         
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        if self.frame_index < len(self.frames):
+            self.image = self.frames[int(self.frame_index)]
+        else:
+            self.active =False
+            self.frame_index = 0
+    
+    def update(self, dt):
+        if self.active:
+            self.animate(dt)
+        else:
+            if randint(0, 2000) == 1: # animates the hearts randomly 
+                self.active = True
