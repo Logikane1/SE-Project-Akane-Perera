6770cf42015082c970c95524b2c48b748bb6d29e
“Logikane1” <akane.perera@education.nsw.gov.augit config --global user.name logikane1>
2025-06-06 13:37:59 +1000

    sky generated (partially)

--- Code/groups.py
+++ Code/groups.py
@@ -1,18 +1,23 @@
 from gameSettings import *
+from sprites import Sprite
 
 class AllSprites(pygame.sprite.Group):
     def __init__(self, width, height, bg_tile = None):
         super().__init__()
         self.displaySurface = pygame.display.get_surface()
         self.offset = vector()
-        self.width, self.height = width, height
+        self.width, self.height = width * TILE_SIZE, height * TILE_SIZE
+        
+        if bg_tile:
+            for column in range(width):
+                for row in range(height):
+                    x, y = column * TILE_SIZE, row * TILE_SIZE
+                    Sprite((x,y), bg_tile, self, -1)
         
     def draw(self, target_position):
         self.offset.x = -(target_position[0] - WINDOW_WIDTH / 2) # creating the offset for the screen that follows the player 
         self.offset.y = -(target_position[1] - WINDOW_HEIGHT / 2)
         
-    
-    
         for sprite in sorted(self, key = lambda sprite: sprite.z): # returns all sprites inside of it / anything drawn before this for loop is in the background (easy to make sky here)
             offset_position = sprite.rect.topleft + self.offset
             self.displaySurface.blit(sprite.image, offset_position)
--- Code/level.py
+++ Code/level.py
@@ -16,10 +16,15 @@ class Level:
         tmx_level_properties = tmx_map.get_layer_by_name('Data')[0].properties
         if tmx_level_properties['bg']:
             bg_tile = level_frames['bg_tiles'][tmx_level_properties['bg']]
-            print(bg_tile)
+        else:
+            bg_tile = None
             
         #groups
-        self.allSprites = AllSprites(self.level_width, self.level_bottom)
+        self.allSprites = AllSprites(
+            width = tmx_map.width, 
+            height = tmx_map.height,
+            bg_tile = bg_tile)
+        
         self.collisionSprites = pygame.sprite.Group()
         self.semicollisionSprites = pygame.sprite.Group()
         self.damageSprites = pygame.sprite.Group()
